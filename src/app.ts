718f99d40edc1c707eebfa74713c62ff79cd2a3d
Lindell Carter <lindellcarternyc@gmail.com>
2018-09-06 15:42:17 -0400

    update app to serve frontend client

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
 import * as express from 'express'
+import { join as joinPath } from 'path'
 
 import api from './api'
 
-const app = express()
-app.get('/api/convert?:input', api)
+// App Constants
+const PUBLIC_DIR = joinPath(process.cwd(), 'public')
 
+// Create application
+const app = express()
+
+// Mount middleware
+app.use(express.static(PUBLIC_DIR))
+
+app.get('/api/convert?:input', api)
+app.get('/', (_, res) => {
+  res.sendFile(joinPath(PUBLIC_DIR, 'index.html'))
+})
 export default app
