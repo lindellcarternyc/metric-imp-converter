da204298508ce234eaee331e5c7a6788db6eda91
Lindell Carter <lindellcarternyc@gmail.com>
2018-09-06 13:39:14 -0400

    add convertor and return results

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,6 @@
+import convert from './converter'
 import parseInput from './parse-input'
+import { APIResponseData } from './types'
 
 interface APIRequest {
   query: {
@@ -15,11 +17,17 @@ const api = (req: APIRequest, res: ApiResponse) => {
   const { input } = req.query
   
   try {
-    const { amount, unit } = parseInput(input)
-    return res.json({
-      initNum: amount,
-      initUnit: unit
-    })
+    const initialValue = parseInput(input)
+    const returnValue = convert(initialValue)
+
+    const data: APIResponseData = {
+      initNum: initialValue.amount,
+      initUnit: initialValue.unit.shortName,
+      returnUnit: returnValue.unit.shortName,
+      returnNum: returnValue.amount,
+      string: `${initialValue.amount} ${initialValue.unit.displayName} converts to ${returnValue.amount} ${returnValue.unit.displayName}`
+    }
+    return res.json(data)
   } catch (err) {
     return res.send(err.message)
   }
