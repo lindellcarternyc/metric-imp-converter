6e35457a26a3b1b5b9f2a2cfd299ec914f13d3f1
Lindell Carter <lindellcarternyc@gmail.com>
2018-09-05 21:47:32 -0400

    remove dummy routes

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,8 @@
 import * as express from 'express'
 
 import api from './api'
-import hello from './hello'
 
 const app = express()
 app.get('/api/convert?:input', api)
-app.get('/', hello)
 
 export default app
